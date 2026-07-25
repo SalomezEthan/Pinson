63de7ae732923a3d46d1194bae32380075c9f1af
Salomez Ethan <salomez-ethan@outlook.fr>
2026-07-25 06:14:15 +0200

    chore : use native GUID now

--- Pinson.Core/Domain/Exceptions/InvalidIdLengthException.cs
+++
@@ -1,8 +0,0 @@
-﻿using Pinson.Core.Domain.Constants;
-
-namespace Pinson.Core.Domain.Exceptions
-{
-    public class InvalidIdLengthException() : Exception($"L'id doit faire {Limits.ID_LENGTH} caractères.")
-    {
-    }
-}
