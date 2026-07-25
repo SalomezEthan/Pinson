e4f5f3184e1ae6a52add9c62674d5945fb4aa3dd
Salomez Ethan <salomez-ethan@outlook.fr>
2026-07-25 18:10:23 +0200

    feat : minimal picture entity

---
+++ Pinson.Core/Domain/Entities/Picture.cs
@@ -0,0 +1,12 @@
+﻿namespace Pinson.Core.Domain.Entities
+{
+    public class Picture
+    {
+        public Picture(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
